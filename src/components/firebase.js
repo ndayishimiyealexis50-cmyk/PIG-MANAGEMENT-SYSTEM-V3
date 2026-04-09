4dd818f2ff1f60b0577f3c660ac6ef854ef08ed6
ndayishimiyealexis50-cmyk <ndayishimiyealexis50@gmail.com>
2026-04-09 06:40:33 +0200

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,12 +4,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPasswor
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyCBH-3ql8onw17geHrJXKUwdhN-EVjtg14",
   authDomain: "my-farm-track.firebaseapp.com",
   projectId: "my-farm-track",
-  storageBucket: "my-farm-track.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  storageBucket: "my-farm-track.firebasestorage.app",
+  messagingSenderId: "529287963399",
+  appId: "1:529287963399:web:733a9828916b61f4525ba2",
+  measurementId: "G-064719MRR4"
 };
 
 const app = initializeApp(firebaseConfig);
